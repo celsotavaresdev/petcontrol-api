968b85c28a4e8d62febe2b7b9c492e27768a7311
celsotavares <celsotavares@pm.me>
2023-11-25 18:29:52 -0300

    refactor: rename RegisterRequest to UserRequest

--- src/main/java/com/ongpatinhasquebrilham/petcontrol/api/model/UserRequest.java
+++ src/main/java/com/ongpatinhasquebrilham/petcontrol/api/model/UserRequest.java
@@ -1,6 +1,8 @@
 package com.ongpatinhasquebrilham.petcontrol.api.model;
 
+import com.ongpatinhasquebrilham.petcontrol.core.validation.EnumValue;
 import com.ongpatinhasquebrilham.petcontrol.domain.model.UserRole;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,10 +10,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class RegisterRequest {
+public class UserRequest {
 
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String password;
-    private UserRole role;
+
+    @NotBlank
+    @EnumValue(enumClass = UserRole.class, ignoreCase = true)
+    private String role;
 
 }
