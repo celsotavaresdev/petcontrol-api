3c878535ae7f4b4a0c5b885ae374597a4134237d
celsotavares <celsotavares@pm.me>
2023-11-25 19:17:06 -0300

    refactor: user controller/service

--- src/main/java/com/ongpatinhasquebrilham/petcontrol/api/controller/UserController.java
+++ src/main/java/com/ongpatinhasquebrilham/petcontrol/api/controller/UserController.java
@@ -1,17 +1,22 @@
 package com.ongpatinhasquebrilham.petcontrol.api.controller;
 
+import com.ongpatinhasquebrilham.petcontrol.api.assembler.UserRequestDisassembler;
 import com.ongpatinhasquebrilham.petcontrol.api.assembler.UserResponseAssembler;
+import com.ongpatinhasquebrilham.petcontrol.api.model.UserRequest;
 import com.ongpatinhasquebrilham.petcontrol.api.model.UserResponse;
+import com.ongpatinhasquebrilham.petcontrol.domain.exception.UserAlreadyExistsException;
 import com.ongpatinhasquebrilham.petcontrol.domain.model.User;
+import com.ongpatinhasquebrilham.petcontrol.domain.repository.UserRepository;
 import com.ongpatinhasquebrilham.petcontrol.domain.service.UserService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @AllArgsConstructor
 @RestController
@@ -20,17 +25,60 @@ public class UserController {
 
 	private UserService userService;
 	private UserResponseAssembler userResponseAssembler;
+	private UserRequestDisassembler userRequestDisassembler;
+	private UserRepository userRepository;
 
 	@GetMapping
-	public ResponseEntity<List<UserResponse>> getAllUsers() {
+	public ResponseEntity<List<UserResponse>> getUsers() {
 		List<User> users = userService.findAll();
 		return ResponseEntity.ok(userResponseAssembler.toCollectionModel(users));
+	}
+
+	@PostMapping
+	public ResponseEntity<UserResponse> saveUser(@Valid @RequestBody UserRequest userRequest) {
+		if (Objects.nonNull(this.userRepository.findByUsername(userRequest.getUsername()))) {
+			throw new UserAlreadyExistsException(String.format("Já existe um cadastro de usuário com o nome %s", userRequest.getUsername()));
+		}
+
+		User convertedUser = userRequestDisassembler.toDomainObject(userRequest);
+		convertedUser.setPassword(new BCryptPasswordEncoder().encode(userRequest.getPassword()));
+
+		User newUser = this.userService.save(convertedUser);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(userResponseAssembler.toModel(newUser));
 	}
 
 	@GetMapping("/{userId}")
 	public ResponseEntity<UserResponse> getUser(@PathVariable Long userId) {
 		User currentUser = userService.find(userId);
 		return ResponseEntity.ok(userResponseAssembler.toModel(currentUser));
+	}
+
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserResponse> updateUser(@PathVariable Long userId, @RequestBody UserRequest userRequest) {
+		User currentUser = userService.find(userId);
+
+		User foundUser = (User) this.userRepository.findByUsername(userRequest.getUsername());
+		if (Objects.nonNull(foundUser)) {
+			if (!Objects.equals(foundUser.getId(), userId)) {
+				throw new UserAlreadyExistsException(String.format("Já existe um cadastro de usuário com o nome %s", userRequest.getUsername()));
+			}
+		}
+
+		if(Objects.nonNull(userRequest.getPassword())) {
+			userRequest.setPassword(new BCryptPasswordEncoder().encode(userRequest.getPassword()));
+		}
+
+		userRequestDisassembler.copyToDomainObject(userRequest, currentUser);
+		User updatedUser = userService.save(currentUser);
+
+		return ResponseEntity.ok(userResponseAssembler.toModel(updatedUser));
+	}
+
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
+		userService.delete(userId);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/ongpatinhasquebrilham/petcontrol/domain/service/UserService.java
+++ src/main/java/com/ongpatinhasquebrilham/petcontrol/domain/service/UserService.java
@@ -1,8 +1,9 @@
 package com.ongpatinhasquebrilham.petcontrol.domain.service;
 
-import com.ongpatinhasquebrilham.petcontrol.domain.exception.PetNotFoundException;
+import com.ongpatinhasquebrilham.petcontrol.domain.exception.UserNotFoundException;
 import com.ongpatinhasquebrilham.petcontrol.domain.model.User;
 import com.ongpatinhasquebrilham.petcontrol.domain.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,13 +15,23 @@ public class UserService {
 
 	private UserRepository repository;
 
+	@Transactional
+	public User save(User user) {
+		return repository.save(user);
+	}
+
+	@Transactional
+	public void delete(Long userId) {
+		repository.deleteById(userId);
+	}
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
 
 	public User find(Long userId) {
 		return repository.findById(userId)
-				.orElseThrow(() -> new PetNotFoundException(userId));
+				.orElseThrow(() -> new UserNotFoundException(userId));
 	}
 
 }
